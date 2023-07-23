b951147310f13e4efebfa67ebca78572e3613283
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-07-23 22:07:46 +0200

    TECNO-KG5j: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: tecno Model: TECNO KG5j Script version: 3.0.0

---
+++ omni_TECNO-KG5j.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from TECNO-KG5j device
+$(call inherit-product, device/tecno/TECNO-KG5j/device.mk)
+
+PRODUCT_DEVICE := TECNO-KG5j
+PRODUCT_NAME := omni_TECNO-KG5j
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO KG5j
+PRODUCT_MANUFACTURER := tecno
+
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_kg5j_h6126-user 11 RP1A.200720.011 139945 release-keys"
+
+BUILD_FINGERPRINT := TECNO/KG5j-OP/TECNO-KG5j:11/RP1A.200720.011/221028V431:user/release-keys
